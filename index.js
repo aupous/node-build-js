9b873a1c7f2961d50a51d1faa38e4b60234033a2
aupous <tuan.phamvan.m@gmail.com>
2020-09-12 14:25:38 +0700

    feat: config to pass param when start or build vue

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const execa = require("execa");
 
 (async () => {
   try {
-    await execa("npx", ["vue-cli-service", "build"], { cwd: "./frontend" });
+    await execa("yarn", ["build", "--productID=1234", "--shopID=shopName"], {
+      cwd: "./frontend",
+    });
   } catch (error) {
     console.log(error);
   }
